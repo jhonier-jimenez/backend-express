a7fc9e62c8290ac852a18025ac3f553387958d3b
Jhonier Jimenez <jhor091@gmail.com>
2024-07-20 07:08:50 -0500

    Cors added

--- src/app/index.js
+++ src/app/index.js
@@ -7,8 +7,15 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  next();
+});
+
 app.use("/api/v1", movieRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
